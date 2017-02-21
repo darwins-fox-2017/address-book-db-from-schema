b47204e9b66b56c2cb2b98a79db71c2283d093d7
samuelphsinaga <samuelphsinaga@gmail.com>
2017-02-21 19:58:48 +0700

    done

--- main.js
+++ main.js
@@ -2,18 +2,18 @@
 "use strict"
 
 import {Contact} from "./contact.js"
-// import {Groups} from "./group.js"
+import {Group} from "./group.js"
 import {ContactGroup} from "./contact-group.js"
 
 const repl = require('repl')
 
 let contact = new Contact()
-// let groups = new Groups()
+let group = new Group()
 let contactGroup = new ContactGroup()
 
 let start = repl.start('> ')
 
-//contact gropu
+//contact group
 start.context.addCG = contactGroup.addCG
 start.context.editCG = contactGroup.editCG
 start.context.deleteCG = contactGroup.deleteCG
@@ -24,3 +24,10 @@ start. context.addContact = contact.addContact
 start.context.editContact = contact.editContact
 start.context.deleteContact = contact.deleteContact
 start.context.showContact = contact.showContact
+
+//group details
+start.context.addGroup = group.addGroup
+start.context.showGroup = group.showGroup
+start.context.editGroup = group.editGroup
+start.context.deleteGroup = group.deleteGroup
+
