b7006a6794ba5fd236b3322733a34ffe6c1901f1
irwinpratajaya <irwinpratajaya@gmail.com>
2017-02-22 10:06:49 +0700

    done

--- setup.js
+++ setup.js
@@ -1,78 +1,120 @@
-let repl = require('repl')
-let sqlite = require('sqlite3')
-let data = 'addressbook.db'
-let db = new sqlite.Database(data)
+"use strict"
 
-let CONTACT_TABLE = `CREATE TABLE IF NOT EXIST contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, company_name TEXT NOT NULL, phone TEXT NOT NULL, email TEXT NOT NULL);`
-db.serialize(function() {
-  db.run(CONTACT_TABLE,function() {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(`contact table created`);
-    }
-  })
-})
+const sqlite3 = require("sqlite3").verbose();
+const file = "addressbook.db";
+const db = new sqlite3.Database(file);
+const repl = require('repl');
+const fs = require('fs');
+let data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
-let GROUP_TABLE = `CREATE TABLE IF NOT EXIST groups (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL);`
-db.serialize(function() {
-  db.run(GROUP_TABLE,function() {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(`group table created`);
-    }
-  })
-})
+let CREATE_TABLE_CONTACTS = "CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, company_name TEXT, phone TEXT, email TEXT)";
+let CREATE_TABLE_GROUPS = "CREATE TABLE IF NOT EXISTS groups (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL)";
+let CREATE_TABLE_GROUP_CONTACTS = "CREATE TABLE IF NOT EXISTS group_contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, contact_id REFERENCES contacts(id), group_id REFERENCES groups(id))";
+let SEED_DATA_CONTACTS = `INSERT INTO contacts (name, company_name, phone, email) VALUES `;
+let SEED_DATA_GROUPS = `INSERT INTO groups (name) VALUES `;
+let SEED_DATA_GROUP_CONTACTS = `INSERT INTO group_contacts (contact_id, group_id) VALUES `;
 
-let CONTACT_GROUP_TABLE = "CREATE TABLE IF NOT EXISTS contact_groups (id INTEGER PRIMARY KEY AUTOINCREMENT, contacts_id INTEGER, groups_id INTEGER, FOREIGN KEY (contacts_id) REFERENCES contacts(id), FOREIGN KEY (groups_id) REFERENCES groups(id));";
-db.serialize(function() {
-  db.run(CONTACT_GROUP_TABLE, function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(`contact group table created`);
-    }
-  });
-});
-
-function import_json() {
-  const fs = require('fs')
-  var list = JSON.parse(fs.readFileSync('data.json', "utf-8"));
-  for ( let i = 0; i < list.contact.length; i++) {
-    var IMPORT_CONTACT = `INSERT INTO contacts (name, company_name, phone, email) VALUES ('${list.contact[i].name}', '${list.contact[i].company}', '${list.contact[i].phone}', '${list.contact[i].email}')`;
-    db.serialize(function() {
-      db.run(IMPORT_CONTACT, function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('table contact data added');
-        }
-      });
-    });
-  }
-
-  for ( let i = 0; i < list.groups.length; i++) {
-    var IMPORT_GROUPS = `INSERT INTO groups (name) VALUES ('${list.groups[i].name}')`;
-    db.serialize(function() {
-      db.run(IMPORT_GROUPS, function(err) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('table groups data added');
-        }
-      });
-    });
-  }
-
-  for ( let i = 0; i < list.contact_groups.length; i++) {
-    var IMPORT_CONTACT_GROUPS = `INSERT INTO contact_groups (contacts_id, groups_id) VALUES ('${list.contact_groups[i].group_id}', '${list.contact_groups[i].contact_id}')`;
-    db.serialize(function() {
-      db.run(IMPORT_CONTACT_GROUPS, function(err) {
-        (err) ? console.log(err) : console.log('table contact groups data added');
-      });
-    });
+for (var i = 0; i < data.Contacts.length; i++) {
+  if (i < data.Contacts.length - 1) {
+    SEED_DATA_CONTACTS += `('${data.Contacts[i].name}', '${data.Contacts[i].company_name}', '${data.Contacts[i].phone}', '${data.Contacts[i].email}'),`
+  } else {
+    SEED_DATA_CONTACTS += `('${data.Contacts[i].name}', '${data.Contacts[i].company_name}', '${data.Contacts[i].phone}', '${data.Contacts[i].email}')`
   }
 }
 
-import_json()
+for (var i = 0; i < data.Groups.length; i++) {
+  if (i < data.Groups.length - 1) {
+    SEED_DATA_GROUPS += `('${data.Groups[i].name}'),`
+  } else {
+    SEED_DATA_GROUPS += `('${data.Groups[i].name}')`
+  }
+}
+
+for (var i = 0; i < data.Group_Contacts.length; i++) {
+  if (i < data.Group_Contacts.length - 1) {
+    SEED_DATA_GROUP_CONTACTS += `('${data.Group_Contacts[i].contact_id}', '${data.Group_Contacts[i].group_id}'),`
+  } else {
+    SEED_DATA_GROUP_CONTACTS += `('${data.Group_Contacts[i].contact_id}', '${data.Group_Contacts[i].group_id}')`
+  }
+}
+
+let createTableContacts = () => {
+  db.serialize(function() {
+    db.run(CREATE_TABLE_CONTACTS, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('CREATE TABLE CONTACTS');
+      }
+    })
+  })
+}
+
+let createTableGroups = () => {
+  db.serialize(function() {
+    db.run(CREATE_TABLE_GROUPS, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('CREATE TABLE GROUPS');
+      }
+    })
+  })
+}
+
+let createTableGroupContacts = () => {
+  db.serialize(function() {
+    db.run(CREATE_TABLE_GROUP_CONTACTS, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('CREATE TABLE GROUP CONTACTS');
+      }
+    })
+  })
+}
+
+let seedDataContacts = () => {
+  db.serialize(function() {
+    db.run(SEED_DATA_CONTACTS, function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log('SEED DATA CONTACTS');
+      }
+    })
+  })
+}
+
+let seedDataGroups = () => {
+  db.serialize(function() {
+    db.run(SEED_DATA_GROUPS, function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log('SEED DATA GROUPS');
+      }
+    })
+  })
+}
+
+
+let seedDataGroupContacts = () => {
+  db.serialize(function() {
+    db.run(SEED_DATA_GROUP_CONTACTS, function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log('SEED DATA GROUP CONTACTS');
+      }
+    })
+  })
+}
+
+let start = repl.start('> ')
+start.context.createTableContacts = createTableContacts
+start.context.createTableGroups = createTableGroups
+start.context.createTableGroupContacts = createTableGroupContacts
+start.context.seedDataContacts = seedDataContacts
+start.context.seedDataGroups = seedDataGroups
+start.context.seedDataGroupContacts = seedDataGroupContacts
